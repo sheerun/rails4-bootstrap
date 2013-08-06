4cd86aa7244250ffb701e0ee5aa079348206a6bc
Adam Stankiewicz <sheerun@sher.pl>
2013-08-06 23:27:47 +0200

    Enable helper and controller specs again, fixes #13

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ class Application < Rails::Application
   config.generators do |g|
     g.test_framework :rspec, fixture: false
     g.view_specs false
-    g.helper_specs false
-    g.controller_specs false
     g.integration_specs false
     g.stylesheets = false
     g.javascripts = false
