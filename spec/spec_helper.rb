470333a6845f464e3b4a028fc95e3ae0cc5a9314
Adam Stankiewicz <sheerun@sher.pl>
2014-01-11 03:38:00 +0100

    Update Travis script to handle migrations, fixes #32

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '../config/environment.rb')
 
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
