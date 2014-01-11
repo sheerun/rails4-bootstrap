a834e16928c815ae8d94e01c87cc081436f1f686
Adam Stankiewicz <sheerun@sher.pl>
2014-01-11 03:18:33 +0100

    Load Rails test environment in spec_helper, fixes #12

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+ENV['RAILS_ENV'] = 'test'
+require File.join(File.dirname(__FILE__), '../config/environment.rb')
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
